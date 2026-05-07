3c41c5433c67de5bc788ce1f020da791d07a4f0a
JulieCurran99 <juc005@edu.zealand.dk>
2026-05-07 14:59:13 +0200

    Update ShelterRegister.cs

--- ClassLib_Shelter/Classes/ShelterRegister.cs
+++ ClassLib_Shelter/Classes/ShelterRegister.cs
@@ -2,5 +2,77 @@ namespace ClassLib_Shelter.Classes;
 
 public class ShelterRegister
 {
-    
+#region Instance fields
+        private List<Shelter> _register;
+        private string _name;
+ #endregion
+
+
+ #region Constructor
+public string Name
+		{
+			_name = "";
+		}
+public string Name(string name)
+		{
+			_name = name;
+		}
+#endregion
+
+            
+
+#region Properties
+        public string Name 
+        { 
+            get { return _name; } 
+            set { _name = value; } 
+        }
+        public List<Shelter> Register 
+        { 
+            get { return _register; } 
+            set { _register = value; } 
+        } 
+#endregion
+
+
+        #region Methods
+        public List<Shelter> GetAllShelters() 
+        { 
+            return new List<Shelter>(_register); 
+        }
+        public void AddShelter(Shelter newShelter)
+        {
+            _register.Add(newShelter);
+        }
+        public Shelter RemoveShelter(int shelterId)
+        {
+            _register.Remove(GetShelter(shelterId));
+        }
+
+        public Shelter GetShelter(int shelterId) 
+        {
+            foreach (Shelter shelter in _register)
+            {
+                if (shelterId == Shelter.Id) { return shelter; }
+            }
+            return null;
+        }
+
+        public Shelter UpdateShelter(int shelterId, Shelter updatedShelter)
+        {
+            Shelter shelter = GetShelter(shelterId);
+
+            if (shelter != null)
+            {
+                shelter.ShelterId = updatedShelter.ShelterId;
+                shelter.Name = updatedShelter.Name;
+                shelter.Geolocation = updatedShelter.Geolocation;
+                shelter.Place = updatedShelter.Place;
+                shelter.MaximumCapacity = updatedShelter.MaximumCapacity;
+            }
+            return shelter;
+        }
+
+        #endregion
+
 }
