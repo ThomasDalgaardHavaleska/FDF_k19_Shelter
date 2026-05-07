b919074d78b244c1214abd3d85a81d5aebcb1f7c
Elisabeth Holløse Rantzau Lagermann <ell004@edu.zealand.dk>
2026-05-07 14:59:10 +0200

    Elisabeth har oprettet klassen User og tilføjet instansfelter, konstruktører, properties og metoder.

--- ClassLib_Shelter/Classes/User.cs
+++ ClassLib_Shelter/Classes/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ClassLib_Shelter.Classes
@@ -8,22 +9,110 @@ namespace ClassLib_Shelter.Classes
     {
         //Instansfelter
 
+        private int _userId;
+        private string _fullname;
+        private string _email;
+        private string _role;
+        private DateTime _dateOfCreation;
+        private District _districtAssociation;
+
+
 
 
         //Konstruktører
 
+        //default konstruktør
 
+        public User()
+        {
+            _userId = 0;
+            _fullname = "";
+            _email = "";
+            _role = "";
+            _dateOfCreation = DateTime.Now;
+            _districtAssociation = null; //Vi skal vel have at en bruger skal have en default-kreds fra starten af?
+
+        }
+
+        public User(int userId, string fullname, string email, string role, DateTime dateOfCreation, District DistrictAssocation)
+        {
+            _userId = userId;
+            _fullname = fullname;
+            _email = email;
+            _role = role;
+            _dateOfCreation = dateOfCreation;
+            _districtAssociation = DistrictAssocation;
+        }
 
 
         //Properties
 
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public string Fullname
+        {
+            get { return _fullname; }
+            set { _fullname = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string Role
+        {
+            get { return _role; }
+            set { _role = value; }
+        }
+
+        public DateTime DateOfCreation
+        {
+            get { return _dateOfCreation;  }
+            set { _dateOfCreation = value; }
+        }
+
+        public District DistrictAssociation
+        { 
+            get { return _districtAssociation; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("As a new user, you need to choose a district");
+
+                    _districtAssociation = value;
+                }
+            }
+                
+        }
 
 
         //Metoder
 
         //To string-metode
 
+        public override string ToString()
+        {
+            return 
+                "User with ID: " + UserId + 
+                ". \n Fullname: " + Fullname + 
+                ". \n Email: " + Email
+                + ". \n Role: " + Role + 
+                ". \n User was created: " + DateOfCreation;
+        }
 
+        //void ChangeDistrict(District newDisctrict) bør ikke være en metode for sig, men gøres under dens property
+        //{
+
+
+
+        //}
 
     }
 }
