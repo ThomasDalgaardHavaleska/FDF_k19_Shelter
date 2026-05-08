07aaf529c9321cd41bd164f35be28f15dcc7b2cc
Ulrik <ulb002@edu.zealand.dk>
2026-05-08 09:13:12 +0200

    small update

--- ClassLib_Shelter/Classes/DistrictRegister.cs
+++ ClassLib_Shelter/Classes/DistrictRegister.cs
@@ -8,7 +8,7 @@ namespace ClassLib_Shelter.Classes
     public class DistrictRegister
     {
         #region Instance fields
-        private List<District> _register;
+        private List<District> _districts;
         private int _id;
         #endregion
 
@@ -20,49 +20,49 @@ namespace ClassLib_Shelter.Classes
             get { return _id; } 
             set { _id = value; } 
         }
-        public List<District> Register 
+        public List<District> Districts 
         { 
-            get { return _register; } 
-            set { _register = value; } 
+            get { return _districts; } 
+            set { _districts = value; } 
         }
         #endregion
         #region Constructor
         public DistrictRegister()
         {
             _id = 0;
-            _register = new List<District>();
+            _districts = new List<District>();
         }
 
         public DistrictRegister(int id, List<District> register)
         {
             _id = id;
-            _register = register; 
+            _districts = register; 
         }
         #endregion
         #region Methods
         public List<District> GetAllDistricts() 
         { 
-            return new List<District>(_register); 
+            return new List<District>(_districts); 
         
         }
 
         public void AddDistrict(District newDistrict)
         {
-            _register.Add(newDistrict);
+            _districts.Add(newDistrict);
         }
 
 
         public void RemoveDistrict(int districtId)
         {
 
-            _register.Remove(GetDistrict(districtId));
+            _districts.Remove(GetDistrict(districtId));
 
 
         }
 
         public District GetDistrict(int districtId) 
         {
-            foreach (District district in _register)
+            foreach (District district in _districts)
             {
                 if (districtId == District.DistrictId) { return district; }
             }
