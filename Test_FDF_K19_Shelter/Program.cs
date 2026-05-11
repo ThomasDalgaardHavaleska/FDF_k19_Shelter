d794be0e526124518ac3634e9e4ab080b15d2d79
Ulrik Herborg Borum <ulb002@edu.zealand.dk>
2026-05-11 15:47:15 +0200

    test

--- Test_FDF_K19_Shelter/Program.cs
+++ Test_FDF_K19_Shelter/Program.cs
@@ -7,7 +7,8 @@ using ClassLib_Shelter.Model;
 
 DateTime DateNow = DateTime.Now;
 
-User user = new User(12,"Lars Olsen", "Lars@olsen.dk", "Felt leder", false, DateNow, null);
+District district = new District(1, "Syd", "Væbner", "København S", "email@example.com", "+45 12 34 56 78");
+User user = new User(12,"Lars Olsen", "Lars@olsen.dk", "Felt leder", false, DateNow, district);
 Booking b = new Booking();
 IRegister<Booking> reg = new BookingRegister();
 
