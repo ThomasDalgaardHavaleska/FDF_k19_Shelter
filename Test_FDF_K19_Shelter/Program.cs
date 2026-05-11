9470069871335187f36f207f78b28d60ebacc309
Ulrik <ulb002@edu.zealand.dk>
2026-05-11 14:39:51 +0200

    space

--- Test_FDF_K19_Shelter/Program.cs
+++ Test_FDF_K19_Shelter/Program.cs
@@ -7,6 +7,7 @@ using ClassLib_Shelter.Model;
 
 DateTime DateNow = DateTime.Now;
 
+
 User user = new User(12,"Lars Olsen", "Lars@olsen.dk", "Felt leder", false, DateNow, null);
 Booking b = new Booking();
 IRegister<Booking> reg = new BookingRegister();
