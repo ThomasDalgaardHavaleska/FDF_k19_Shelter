57cacf6e482f1ff7f8eb649a09c078edfca2f14f
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-05-12 12:31:37 +0200

    Created CommentClass + exceptions.

---
+++ ClassLib_Shelter/Model/Comment.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLib_Shelter.Model
+{
+	public class Comment
+
+	{
+		#region instancefields
+
+		private int _commentId;
+		private string _content;
+		private User _Author;
+		private DateTime _datePublished;
+
+		#endregion
+
+		#region constructor
+		public Comment()
+		{
+			_commentId = 0;
+			_content = "";
+			_Author = null;
+			_datePublished = DateTime.Now;
+		}
+
+		public Comment(int commentId, string content, User author)
+		{
+			_commentId = commentId;
+			_content = content;
+			_Author = author;
+		}
+		#endregion
+
+		#region properties
+
+		public int CommentId
+		{
+			get { return _commentId; }
+			set { _commentId = value; }
+		}
+
+		public string Content
+		{
+			get { return _content; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("Content cannot be empty.");
+				}
+				_content = value;
+			}
+		}
+
+		public User Author
+		{
+			get { return _Author; }
+
+		}
+		#endregion
+	}
+}
