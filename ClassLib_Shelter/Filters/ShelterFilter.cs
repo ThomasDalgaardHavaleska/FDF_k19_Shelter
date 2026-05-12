f6689da25935a14df3fa00debb9a24111a240828
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-05-12 11:40:08 +0200

    Added  Shelter Filter Class

---
+++ ClassLib_Shelter/Filters/ShelterFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLib_Shelter.Model;
+
+namespace ClassLib_Shelter.Filters
+{
+	public class ShelterFilter
+	{
+
+		public List<Shelter> FilterLocation(List<Shelter> shelters, string place)
+		{
+			List<Shelter> result = new List<Shelter>();
+
+			foreach (Shelter shelter in shelters)
+			{
+				if (shelter.Place == place)
+				{
+					result.Add(shelter);
+				}
+			}
+			return result;
+		}
+	
+		public List<Shelter> FilterMaxCapacity(List<Shelter> shelters, int maxCapacity)
+		{
+			List<Shelter> result = new List<Shelter>();
+			
+			foreach (Shelter shelter in shelters)
+			{
+				if (shelter.MaximumCapacity >= maxCapacity)
+				{
+					result.Add(shelter);
+				}
+			}
+			return result;
+		}
+		
+		
+		
+		
+		
+		}
+}
