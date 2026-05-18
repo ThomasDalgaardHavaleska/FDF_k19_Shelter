4a48835910c0e31d69a8dce2609b2e49211a8484
Ulrik <ulb002@edu.zealand.dk>
2026-05-18 10:40:00 +0200

    Created RazorPage pages for districts and shelter.

---
+++ WebApplication_FDF_K19_Shelter/Pages/DistrictRegister.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_FDF_K19_Shelter.Pages
+{
+    public class DistrictRegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplication_FDF_K19_Shelter/Pages/Shelter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_FDF_K19_Shelter.Pages
+{
+    public class ShelterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
